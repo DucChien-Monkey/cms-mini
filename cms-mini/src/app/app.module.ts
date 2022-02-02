b0d579e8d03a26d67a9ed198d98c190973d26ca5
DucChien-Monkey <chien.nguyen@hoc10.com>
2022-02-02 15:32:22 +0700

    command

--- cms-mini/src/app/app.module.ts
+++ cms-mini/src/app/app.module.ts
@@ -4,9 +4,12 @@ import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
-  declarations: [AppComponent, HeroesComponent],
+  declarations: [AppComponent, HeroesComponent, LoginComponent, RegisterComponent, ProfileComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
