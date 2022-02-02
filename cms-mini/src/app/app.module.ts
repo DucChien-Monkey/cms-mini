1b3489a396a832c69489ca7560adcf2639a6283d
DucChien-Monkey <chien.nguyen@hoc10.com>
2022-02-02 23:44:35 +0700

    inline-style template-style

--- cms-mini/src/app/app.module.ts
+++ cms-mini/src/app/app.module.ts
@@ -4,9 +4,12 @@ import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { StudentListComponent } from './student-list/student-list.component';
+import { CountryComponent } from './country/country.component';
 
 @NgModule({
-  declarations: [AppComponent, HeroesComponent],
+  declarations: [AppComponent, HeroesComponent, UserListComponent, StudentListComponent, CountryComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
